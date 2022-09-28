46878f235cd03387ec499750abb4d9d9a8524c54
91961 <91961@LAPTOP-ED372UJ1>
2022-09-28 19:04:00 +0530

    third commit: testcase3 added

---
+++ GitProject/src/testcases/TestCase3.java
@@ -0,0 +1,10 @@
+package testcases;
+
+public class TestCase3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is testcase 3");
+	}
+
+}
