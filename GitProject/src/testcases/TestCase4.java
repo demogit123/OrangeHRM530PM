1e6dd0e87dc37c8d788d50184cf3f99eabb33863
anjan <anjan@LAPTOP-OJA3KFO9>
2022-09-28 19:27:02 +0530

    TC4 Updated

--- GitProject/src/testcases/TestCase4.java
+++ GitProject/src/testcases/TestCase4.java
@@ -4,7 +4,8 @@ public class TestCase4 {
 
 	public static void main(String[] args) 
 	{
-     System.out.println("Test Csae 4");
+     System.out.println("Test Case 4");
+     System.out.println("Updated....");
 	}
 
 }
